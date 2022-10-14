5e980e4c69969bf8e873cc88bc455565afa67bfc
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-10-14 13:44:36 +0200

    added types and database entity

--- src/likes/likes.entity.ts
+++ src/likes/likes.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { LikeEntityTypes } from '../../types';
-import { Users } from '../users/users.entity';
+import { UsersEntity } from '../users/users.entity';
 import { Posts } from '../posts/posts.entity';
 
 @Entity()
@@ -8,8 +8,8 @@ export class Likes extends LikeEntityTypes {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ManyToOne((type) => Users, (entity) => entity.id)
-  userId: Users;
+  @ManyToOne((type) => UsersEntity, (entity) => entity.id)
+  userId: UsersEntity;
 
   @ManyToOne((type) => Posts, (entity) => entity.id)
   postId: Posts;
